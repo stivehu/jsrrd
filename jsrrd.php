39f382073d733b8d09b9d8693fc87e9fd8860212
root <root@web1.bekesikisterseg.eu>
2015-11-24 10:58:51 +0100

    init release

--- jsrrd.php
+++ jsrrd.php
@@ -1,14 +1,27 @@
 <?php
 
-namespace \stivehu\widgets;
+namespace stivehu\widgets;
+
+use yii\base\Widget;
+use yii\web\View;
 
 /**
- * This is just an example.
+ * jsrrd wrapper class
  */
-class Jsrrd extends \yii\base\Widget
-{
-    public function run()
-    {
-        return "Hello!";
+class Jsrrd extends Widget {
+
+    public function run() {
+        $this->registerAssets();
+        $this->registerDrawScript();
+        return $this->render('YiiJsrrd');
     }
+
+    private function registerDrawScript() {
+        $this->getView()->registerJs("draw();", View::POS_END);
+    }
+
+    private function registerAssets() {
+        JsrrdAsset::register($this->getView());
+    }
+
 }
